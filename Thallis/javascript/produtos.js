af7e511836a95864b3f189631d73d13f7f84aec9
thallisabreu <86570847+thallisabreu@users.noreply.github.com>
2021-07-04 22:03:41 -0300

    Add files via upload

---
+++ Thallis/javascript/produtos.js
@@ -0,0 +1,86 @@
+const produtos = [
+  {
+    id: 1,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 01',
+    description: 'Descricão do produto 01',
+    value: 100.00
+  },
+  {
+    id: 2,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 02',
+    description: 'Descricão do produto 02',
+    value: 20.00
+  },
+  {
+    id: 3,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 03',
+    description: 'Descricão do produto 03',
+    value: 30.00
+  },
+  {
+    id: 4,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 04',
+    description: 'Descricão do produto 04',
+    value: 40.00
+  },
+  {
+    id: 5,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 05',
+    description: 'Descricão do produto 05',
+    value: 50.00
+  },
+  {
+    id: 6,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 06',
+    description: 'Descricão do produto 06',
+    value: 60.00
+  },
+  {
+    id: 7,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 07',
+    description: 'Descricão do produto 07',
+    value: 70.00
+  },
+  {
+    id: 8,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 08',
+    description: 'Descricão do produto 08',
+    value: 80.00
+  },
+  {
+    id: 9,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 09',
+    description: 'Descricão do produto 09',
+    value: 90.00
+  },
+  {
+    id: 10,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 10',
+    description: 'Descricão do produto 10',
+    value: 200.00
+  },
+  {
+    id: 11,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 11',
+    description: 'Descricão do produto 11',
+    value: 310.00
+  },
+  {
+    id: 12,
+    image: 'https://picsum.photos/200',
+    title: 'Produto 12',
+    description: 'Descricão do produto 12',
+    value: 410.00
+  }
+]
